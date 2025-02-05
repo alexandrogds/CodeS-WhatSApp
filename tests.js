dd3b294cdb4d482095a59458aebe5ef1b703dd36
Alex <alexandrogonsan@outlook.com>
2025-02-05 18:16:39 -0400

    exclude file of tests

--- tests.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-import { appendFile, readFile, writeFile } from './myfs.js'
-
-dotenv.config({ path: './.env' });
-console.log(process.env.WINNERS_ARENA_SC)
-let data = await readFile(process.env.WINNERS_ARENA_SC);
-console.log(data)
-let winnerData = JSON.parse(data)
-console.log(winnerData)
