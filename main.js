7bc6d84efadb994efa28e56080e7c83633b4a58a
Alex <alexandrogonsan@outlook.com>
2025-02-06 00:11:36 -0400

    bk antes de colocar o codigo para resolver vidas iguais com debuffs/buffs

--- main.js
+++ main.js
@@ -2,46 +2,23 @@
 
 const qrcode = require('qrcode-terminal')
 const dotenv = require('dotenv')
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js')
-const analyzeResults = require('./analyzeResults.js')
-const calcularProximidade = require('./calcularProximidade.js')
-const getNicksOfMessage = require('./getNicksOfMessage.js')
-const { registrar_pontos } = require('./registrarPontos.js') // Named const
-const registrarPontosHelper = require('./registrarPontosHelper.js')
 const error = require('./error.js')
-const getTelefoneS = require('./getTelefoneS.js')
-const contarUnicodeEmojis = require('./contarUnicodeEmojis.js')
-const { handleOneFlag, handleTwoFlags } = require('./fss.js') // const the functions
-const { processDataAndSave } = require('./processDataAndSave.js') // const the functions
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js')
+const { calcularProximidade } = require('./arena_sc/calcularProximidade.js')
+const { getNickSOfMessage } = require('./arena_sc/getNickSOfMessage.js')
+const { getTelefoneS } = require('./arena_sc/getTelefoneS.js')
+const { contarUnicodeEmojis } = require('./arena_sc/contarUnicodeEmojis.js')
+const { handleOneFlag, handleTwoFlags, handle3Flags, handle4Flags } = require('./arena_sc/determineWinner.js') // const the functions
 const { appendFile, readFile, writeFile } = require('./myfs.js') // const the functions
-const generateTable = require('./generateTable.js')
-const determineWinner = require('./determineWinner.js')
-const getWinnersAndTheirFights = require('./getWinnersAndTheirFights.js')
+const { generateTable } = require('./arena_sc/generateTable.js')
+const { getWinnersAndTheirFights } = require('./arena_sc/getWinnersAndTheirFights.js')
 
 dotenv.config({ path: './.env' });
 
 // Initialize ALL global variables with appropriate default values.
 // It's crucial to initialize these outside the Comandos function
 // so that their values are not reset every time Comandos is called.
-let phoneS
-let juizes_e_os_players_da_luta = {};
-let nicks_e_seus_telefones = [];
-let data_hora_da_ultima_tabela = null;
-let aux_dia_semana_numero = null;
-let nicks_e_suas_quantidade_de_vitorias = {};
-let datas_nicks_e_suas_quantidade_de_vitorias = {};
-let meses_e_nicks_com_suas_quantidade_de_vitorias = {};
-let periodo_mensal = null;
-let periodo_semanal = null;
-let dia_semana_numero = null;
-let quem_enviou_a_mensagem_anterior__juiz = null;
-let data_hora_objeto = null;
-let dia = null;
-let mes = null;
-let ano = null;
-// let nicks = []; // Initialize nicks array
-let nickSOfMessage = []; // Initialize nickSOfMessage array
-let flag = '';
+let phoneS; let flag
 
 // Create a new client instance
 const client = new Client({ authStrategy: new LocalAuth() });
@@ -81,7 +58,8 @@ function removeFirst9After55(str) {
 
 // Function to initialize data (nicks, telefones, etc.)
 async function initializeData() {
-    phoneS = await getTelefoneS();
+    phoneS = await getTelefoneS()
+    flag = ''
 }
 
 initializeData()
@@ -99,7 +77,8 @@ async function Comandos(message) {
     }
     if (message.from === group_arena_sc && message.body != '.tag') {
         // ARENA SC CONTAGEM DE LUTAS EM TEMPO REAL
-        console.log(qwe)
+        console.log('into comandos')
+        console.log('ARENASC')
         console.log(group_arena_sc)
         console.log(`message.body:`, message.body)
         console.log(`message.from:`, message.from)
@@ -111,29 +90,29 @@ async function Comandos(message) {
         // console.log(`message.isGroupMsg:`, message.isGroupMsg)
         // console.log(`message.isMedia:`, message.isMedia)
         // console.log(`message.isNotification:`, message.isNotification)
-        process.stdout.write(`\n.bool: ${message.from === process.env.TEST_GROUP}`);
-        process.stdout.write(new Date().toLocaleTimeString());
+        console.log(`\n.bool: ${message.from === process.env.TEST_GROUP}`);
+        console.log(new Date().toLocaleTimeString());
         // process.stdout.write(' ', typeof String.raw`${message.body}`)
         
-        let winner
+        let winner; flag = ''
         let dataHoraUser = {  // Create the object
 			data_hora: message.timestamp,
 			usuario: message.author || message.from // Use author if available, otherwise from
 		};
-        process.stdout.write('entrou1.');
         
         //Regular expression matching numbers with 1 to 3 digits not surrounded by other digits
         const regex = /(?<!\d)[0-9]{1,3}(?!\d)/g;
         const numbers = message.body.match(regex) || [];
         process.stdout.write(`Extracted Numbers: ${numbers}.`);
     
-        const nickSOfMessage = await getNicksOfMessage(message);
+        const nickSOfMessage = await getNickSOfMessage(message);
 		const nick_count = nickSOfMessage.length; // Get nick_count here
 
         if (numbers.length < 4) {
             return
         } else if ( nick_count == 2) {
             const resultados = calcularProximidade(message.body, nickSOfMessage)
+            // resultados = [ [ 'noelle', [ 0, 400 ] ], [ 'acácia', [ 200, 400 ] ] ]
             console.log(resultados)
             // let allStatuS = resultados[nickSOfMessage[0]]['status'].concat(resultados[nickSOfMessage[1]]['status'])
             // let resultsProcess = allStatuS.reduce((map) => { if (map.has(0)) { map.set(0, map.get(0) + 1); } else { map.set(0, 1); } return map; }, new Map())
@@ -160,8 +139,10 @@ async function Comandos(message) {
 
 
 
-
-            else if ((resultados[0][1].length !== 2 || resultados[1][1].length !== 2) && flag != '') {
+            // 0 && mana; _ 0 is first|top player
+            // 1 && mana; _ 0 is second|botton player _ mana significa que o debuff|buff está na mana, após o número de mana
+            // 0 && vida; _ vida significa que o debuff|buff está na vida, após o número de vida
+            else if ((resultados[0][1].length !== 2 || resultados[1][1].length !== 2)) {
                 if (resultados[0][1].length === 3) {
                     if (resultados[0][1].filter(x => x === Math.min(...resultados[0][1])).length === 1) { // Count occurrences of min value
                         const minIndex = resultados[0][1].indexOf(Math.min(...resultados[0][1]));
@@ -173,12 +154,16 @@ async function Comandos(message) {
                             error(message.body, '555111', `efeito sobre vida deslocado`); // Use JSON.stringify
                             return null; // Or handle error as needed
                         }
+                    } else {
+                        error(message.body, '6661', 'PARTE NA LUTA COM BUFFS OU DEBUFFS COM OS NUMEROS MINIMOS DIFERENTES DE 1')
+                        return
                     }
                 } else if (resultados[0][1].length === 4) {
                     if (resultados[0][1][1] != 0 && resultados[0][1][3] != 0) {
                         flag += '0 && mana; 0 && vida;';
                     } else {
                         error(message.body, '000011', `ESSA LUTA TEM SIMULTANEAMENTE DOIS EFEITOS SECUNDARIOS JUNTOS AO LIFE E CHAKRA`); // Use JSON.stringify
+                        return
                     }
                 } else {
                     error(message.body, '666111', `ESSA LUTA TEM SIMULTANEAMENTE DOIS EFEITOS SECUNDARIOS JUNTOS AO LIFE E CHAKRA`); // Use JSON.stringify
@@ -197,49 +182,64 @@ async function Comandos(message) {
                             return null;// Or handle error as needed
                         }
                     }
-                }else if(resultados[1][1].length === 4){
-                    // TODO
+                } else if(resultados[1][1].length === 4) {
+                    if (resultados[1][1][1] != 0 && resultados[1][1][3] != 0) {
+                        flag += '1 && mana; 1 && vida;';
+                    } else {
+                        error(message.body, '098456', `ESSA LUTA TEM SIMULTANEAMENTE DOIS EFEITOS SECUNDARIOS JUNTOS AO LIFE E CHAKRA`); // Use JSON.stringify
+                        return
+                    }
                 } else {
                     error(message.body, '777111', `ESSA LUTA TEM SIMULTANEAMENTE DOIS EFEITOS SECUNDARIOS JUNTOS AO LIFE E CHAKRA`); // Use JSON.stringify
                     return
                 }
+
+                if (resultados[0][1][0] === 0 && resultados[1][1][0] === 0) {
+                    console.log('ENTROU<-123')
+                    const a99 = flag.split(';').filter(x => x !== '');
+                    if (a99.length === 1) {
+                        console.log('ENTROU<- ONE FLAGS')
+                        winner = handleOneFlag(a99, resultados);
+                    } else if (a99.length === 2) {
+                        console.log('ENTROU<-TWO FLAGS')
+                        winner = handleTwoFlags(a99, resultados);
+                    } else if (a99.length === 3) {
+                        console.log('ENTROU<-TWO FLAGS')
+                        winner = handle3Flags(a99, resultados);
+                    } else if (a99.length === 4) {
+                        console.log('ENTROU<-TWO FLAGS')
+                        winner = handle4Flags(a99, resultados);
+                    }
+                } else {
+                    determinar_vencedor_com_life_diferentes()
+                }
             } else if (resultados[0][1] && resultados[1][1] && resultados[0][1].join("") === resultados[1][1].join("") && (resultados[0][1].includes(0) || resultados[1][1].includes(0))) {
                 error(message.body, '22', 'TRATAR PARA PEGAR A TABELA ANTERIOR PARA DECIDIR QUEM VENCE.\nRESULTADOS = ' + JSON.stringify(resultados));
-            } else if (flag && resultados[0][1] && resultados[1][1] && resultados[0][1][0] === 0 && resultados[1][1][0] === 0) {
-                console.log('ENTROU<-123')
-                const a99 = flag.split(';').filter(x => x !== '');
-                if (a99.length === 1) {
-                    winner = handleOneFlag(a99, resultados, telefones, juizes_e_os_players_da_luta, quem_enviou_a_mensagem_anterior__juiz, nicks_e_seus_telefones, data_hora_da_ultima_tabela, dia, mes, ano, nicks, data_hora_objeto, dia_semana_numero, aux_dia_semana_numero, nicks_e_suas_quantidade_de_vitorias, datas_nicks_e_suas_quantidade_de_vitorias, meses_e_nicks_com_suas_quantidade_de_vitorias, periodo_mensal, periodo_semanal, nickSOfMessage, registrarPontosHelper); // Pass necessary variables
-                } else if (a99.length === 2) {
-                    winner = handleTwoFlags(a99, resultados, telefones, juizes_e_os_players_da_luta, quem_enviou_a_mensagem_anterior__juiz, nicks_e_seus_telefones, data_hora_da_ultima_tabela, dia, mes, ano, nicks, data_hora_objeto, dia_semana_numero, aux_dia_semana_numero, nicks_e_suas_quantidade_de_vitorias, datas_nicks_e_suas_quantidade_de_vitorias, meses_e_nicks_com_suas_quantidade_de_vitorias, periodo_mensal, periodo_semanal, nickSOfMessage, registrarPontosHelper); // Pass necessary variables
-                }
-                flag = null; // Reset flag
-            }  else if (resultados[0][1] && resultados[1][1]) {
-                if (flag && resultados[0][1][0] === 0) {
-                    winner = resultados[1][0]
-                } else if (flag && resultados[1][1][0] === 0) {
-                    winner = resultados[0][0]
-                } else if (resultados[0][1][0] === 0 && resultados[1][1][0] === 0) {  // Both vida are 0
+            } else if (resultados[0][1] && resultados[1][1]) {
+                if (resultados[0][1][0] === 0 && resultados[1][1][0] === 0) {  // Both vida are 0
                     if (resultados[0][1][1] < resultados[1][1][1]) {
                         winner = resultados[1][0]
                     } else if (resultados[0][1][1] > resultados[1][1][1]) {
                         winner = resultados[0][0]
                     } else {
-                        // Handle tie or error (when both mana are also equal)
-                        error(msg, numbers, dataHoraUser, '33', `IMPLEMENTAR PARA SE O CHAKRA FOR IGUAL PARA CONSULTAR NA TABELA ANTERIOR.\nRESULTADOS = ${resultados}`)
-                    }
-                } else if (resultados[0][1][0] === 0) {
+                        error(message.body, numbers, dataHoraUser, '33', `IMPLEMENTAR PARA SE O CHAKRA FOR IGUAL PARA CONSULTAR NA TABELA ANTERIOR.\nRESULTADOS = ${resultados}`)
+                        return
+                    }                } else if (resultados[0][1][0] === 0) {
+                    console.log('ENTROU 4582136')
                     winner = resultados[1][0]
-                } else if (resultados[1][1][0] === 0) {
-                    winner = resultados[0][0]
+                } else {
+                    function determinar_vencedor_com_life_diferentes() {
+                        if (resultados[0][1][0] === 0) {
+                            return resultados[1][0]
+                        } else if (resultados[1][1][0] === 0) {
+                            return resultados[0][0]
+                        }
+                    }
+                    determinar_vencedor_com_life_diferentes()
                 }
             }
         } else if (nick_count === 1) {
             error(message.body, '0', `Pegar o nick da placa abaixo diferente do nick: ${aux_nick}`)
-            console.error("\n\nID=0");
-            console.error(`\nPegar o nick da placa abaixo diferente do nick: ${aux_nick}\n${message.body}\n`);//verificar se aux_nick existe
-            // Handle the error as needed (e.g., send a message, return) – don't just exit()
-
             return;
         
         } else if (nick_count === 0 && message.body && contarUnicodeEmojis(message.body) < 50) {
@@ -266,7 +266,7 @@ async function Comandos(message) {
 
         let winners = await readFile(process.env.WINNERS_ARENA_SC)
         winners = JSON.parse(winners)
-        console.log('winners = ', winners)
+        //console.log('winners = ', winners)
         const dataToAppend = {
             'winner': winner.toLowerCase(),
             'phone': phoneS[winner.toLowerCase()],
@@ -275,7 +275,7 @@ async function Comandos(message) {
         };
         console.log('dataToAppend = ', dataToAppend)
         winners.push(dataToAppend)
-        console.log('winners = ', winners)
+        //console.log('winners = ', winners)
         await writeFile(process.env.WINNERS_ARENA_SC, JSON.stringify(winners, null, 2)); // Correctly call using await
 
         let winnersAndTheirFights = await getWinnersAndTheirFights(winners)
@@ -283,8 +283,7 @@ async function Comandos(message) {
         let table = await generateTable(winnersAndTheirFights)
 
         console.log('Enviando mensagem.')
-        console.log(typeof table)
-        await client.sendMessage(group, table)
+        await client.sendMessage(group_arena_sc, table)
         console.log('Mensagem enviada.')
     } else if (message.author == process.env.OWNER_NUMBER && message.body.toLowerCase() === process.env.PREFIX + 'tag'.toLowerCase()) {
         // to tag _ marcar todos do grupo.
@@ -351,10 +350,25 @@ async function Comandos(message) {
         group_id = process.env.GROUP_TO_SEND_CONTACTS_TO_RECRUIT_TO_THATY
         await send_file_content_to_group_mentioning_numbers_of_content(file_path, group_id)
     } else {
+        let group_dragon_gakure
+        if (process.env.DEBUG) {
+            group_dragon_gakure = process.env.TEST_GROUP
+        } else if (!process.env.DEBUG) {
+            group_dragon_gakure = process.env.DRAGON_GAKURE
+        }
         // eventos zarcovi
-        if (message.author != process.env.BOT_NUMBER || message.to != process.env.DRAGON) {
+        if (message.author != process.env.BOT_NUMBER || message.to != group_dragon_gakure) {
             return
         }
+
+        let events_menu = '1 - 📝 Evento: História Contínua 📝'
+
+        if (message.body.toLowerCase() === process.env.PREFIX + 'Menu'.toLowerCase()) {
+            await client.sendMessage(group_dragon_gakure, events_menu);
+        } else if (message.body.toLowerCase() == '1') {
+            
+        }
+
         // 📝 Evento: História Contínua 📝
         // prompts openai
         // me de uma frase que inicia uma historia. somente retorne a frase que inicia a historia
@@ -384,8 +398,8 @@ client.on('message_create', async message => {
         // console.log(`message.isGroupMsg:`, message.isGroupMsg)
         // console.log(`message.isMedia:`, message.isMedia)
         // console.log(`message.isNotification:`, message.isNotification)
-        process.stdout.write(`.bool: ${message.from === process.env.TEST_GROUP}`);
-        process.stdout.write(new Date().toLocaleTimeString());
+        console.log(`.bool: ${message.from === process.env.TEST_GROUP}`);
+        console.log(new Date().toLocaleTimeString());
         // process.stdout.write(' ', typeof String.raw`${message.body}`)
     }
     
